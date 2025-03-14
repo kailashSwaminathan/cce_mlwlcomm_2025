edd3aa7516cb7b33739a494a91d29ff694315781
kailashSwaminathan <kailashs_in@yahoo.com>
2025-03-14 05:37:27 +0530

    More added

--- quiz/quiz-1/quiz_1.py
+++ quiz/quiz-1/quiz_1.py
@@ -5,6 +5,11 @@ import numpy as np
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
+def zero_threshhold(y):
+    if isinstance(y, np.ndarray):
+        return [0 if i < 0 else 1 for i in y]
+    else:
+        return 0 if y < 0 else 1
 
 def support_vm(qno):
     """
@@ -117,17 +122,159 @@ def nn1(qno):
             grad = np.round(2*(Ypred - Ytarget), decimals=roff)
             print(f"{qno}. Gradient dL/dYpred: {grad}")
 
+def qno12():
+    """
+    """
+    def afn(y):
+        return 0 if y < 40 else 1
+        
+    ws = [[5,15,15],[15,10,15],[25,5,10],[5,20,5]]
+    bs = [10,0,10,15]
+    xs = [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
+    for w,b in zip(ws,bs):
+        err = False
+        print(f"Trying out w: {w}, b: {b}")
+        for i,x in enumerate(xs):
+            y = np.matmul(w,x) + b
+            y = afn(y)
+            if i != 7 and y == 1:
+                err = True
+                break
+        if not err:
+            print(f"GOOD. (w: {w}, b = {b})")
+            #break
+        else:
+            print(f"Not good.")
+            
+def qno13():
+    """
+    """
+    def afn(v):
+        return 0 if v < 0 else 1
+        
+    w = [2, -1.5, 1]
+    xs = [[1, 0, 0], [0, 1, 1]]
+    output = []
+    for x in xs:
+        y = np.matmul(w,x)
+        p = afn(y)
+        output.append(p)
+    print(output)
+    
+def qno14():
+    """
+    """
+    def afn(y):
+        return 0 if y < 0 else 1
+        
+    ws = [[1,1.5],[1,0.5],[1,1.5],[1,-0.5]]
+    bs = [1,-1,-1,1]
+    xs = [[0,0],[0,1],[1,0],[1,1]]
+    for w,b in zip(ws,bs):
+        print(f"Trying out w:{w}, b: {b}")
+        err = False
+        for i,x in enumerate(xs):
+            y = np.matmul(w,x) + b
+            y = afn(y)
+            match (i):
+                case 0:
+                    err = True if y == 1 else err
+                case _:
+                    err = True if y == 0 else err
+            if err:
+                break
+        if err:
+            print("Not Good.")
+        else:
+            print(f"GOOD. w: {w}, b: {b}")
+            
+    
+def qno17():
+    """
+    """
+    def afn(X):
+        return 1/(1 + np.exp(-X))
+        
+    w = [2, -1, 1]
+    b = -1
+    x = [1,1,0]
+    y = np.matmul(w,x) + b
+    y = afn(y)
+    print(y)
+    
+def qno18():
+    """
+    """ 
+    xs = [[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
+    w1 = [[0.5,-1,-1],[-1.5,1,1]]
+    w2 = [1,1]
+    b = [0,-0.5]
+    output = []
+    for x in xs:
+        y = np.matmul(w1,x)
+        y = zero_threshhold(y)
+        y = np.matmul(w2, y) + (-0.5)
+        y = zero_threshhold(y)
+        output.append(y)
+    for x,y in zip(xs, output):
+        print(f"x: {x}, y: {y}")
+
+def qno19():
+    """
+    """
+    x = [1,0,1]
+    w1 = [[0.5,-1,-1],[-1.5,1,1]]
+    w2 = [1,1]
+    y = np.matmul(w1,x)
+    print(y)
+    y = zero_threshhold(y)
+    print(y)
+    y = np.matmul(w2, y) + (-0.5)
+    print(y)
+    y = zero_threshhold(y)
+    print(y)
+    
+def qno24():
+    """
+    """
+    xs = [-1,0,1,2]
+    ys = [0.0319,0.8692,1.9566,3.0343]
+    aas = [2,1,2,1]
+    bs = [1,2,2,1]
+    errs = []
+    for a,b in zip(aas,bs):
+        e = 0.0
+        for x,y in zip(xs, ys):
+            e += (y - (a*x + b))**2
+        errs.append(e)
+    for e,a,b in zip(errs,aas,bs):
+        print(f"Error: {e} for a: {a}, b: {b}")
+    
 
 def main(qno):
     """
     """
     match qno:
-        case 1 | 7 | 8:
+        case 1 | 7 | 8 | 15:
             print("NOT A NUMERICAL QUESTION")
         case 2 | 3 | 4 | 5 | 6:
             support_vm(qno)
         case 9 | 10 | 11:
             nn1(qno)
+        case 12:
+            qno12()
+        case 13:
+            qno13()
+        case 14:
+            qno14()
+        case 17:
+            qno17()
+        case 18:
+            qno18()
+        case 19:
+            qno19()
+        case 24:
+            qno24()
         case _:
             print("NOT IMPLEMENTED!!")
         
